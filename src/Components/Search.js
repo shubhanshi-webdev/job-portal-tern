ad2d54692ef87089cc4acac299e88b080e35c079
sc-shuver <shubhanshi.verma@scryanalytics.com>
2023-11-10 02:21:46 +0530

    mini app job portal

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import JobListing from './JobListing';
 import {addItemsOnSearch} from '../Store/jobListSlice';
